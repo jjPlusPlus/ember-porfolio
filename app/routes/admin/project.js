7806513c86a91d66c1bda6c9cf82e09e20113b49
JJ <justinjcode@gmail.com>
2019-05-22 16:04:37 -0700

    import service injection to admin/project

--- app/routes/admin/project.js
+++ app/routes/admin/project.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from "@ember/service";
 
 export default Route.extend({
+  session: service(),
+
   beforeModel() {
     const session = this.get("session");
     if (!session.isAuthenticated) {
