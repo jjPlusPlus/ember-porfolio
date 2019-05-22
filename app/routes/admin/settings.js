2aee9439679af8aa55ed01b119e24d04c341ff9c
JJ <justinjcode@gmail.com>
2019-05-22 16:13:05 -0700

    initial settings route implementation and controller/template

--- app/routes/admin/settings.js
+++ app/routes/admin/settings.js
@@ -10,5 +10,21 @@ export default Route.extend({
       // user is not logged in; redirect to the index;
       this.transitionTo("/admin/index");
     }
+  },
+
+  model() {
+    return this.store.query("setting", {limit: 1});
+  },
+
+  afterModel(model, controller) {
+    if (model.content.length === 0) {
+      // There was no setting model yet! Let's create one with blank values
+      const newSettingsRecord = this.store.createRecord('setting', {
+        firstName: "Jon",
+        lastName: "Snow"
+      });
+      newSettingsRecord.save();
+      return newSettingsRecord;
+    }
   }
 });
